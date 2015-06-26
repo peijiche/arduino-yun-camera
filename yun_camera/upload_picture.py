a0826d9726662f52daca14f04ad655314af29b2e
Peiji Chen <peijiche@gmail.com>
2015-06-26 07:53:02 -0700

    customized to my settings on dropbox app

--- yun_camera/upload_picture.py
+++ yun_camera/upload_picture.py
@@ -14,18 +14,19 @@ with open(str(sys.argv[1]), "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
 
 # Declare Temboo session and Choreo to upload files
-session = TembooSession('yourSession', 'yourApp', 'yourKey')
+session = TembooSession("ismarthome88", "myFirstApp", "42d33889ea9a45bd8c7bed97a2ccdf17")
 uploadFileChoreo = UploadFile(session)
 
 # Get an InputSet object for the choreo
 uploadFileInputs = uploadFileChoreo.new_input_set()
 
 # Set inputs
-uploadFileInputs.set_AppSecret("yourAppSecret")
-uploadFileInputs.set_AccessToken("yourAccessToken")
+uploadFileInputs.set_AppKey("412wvvs18fz8626")
+uploadFileInputs.set_AppSecret("426ckdalhh3tk3t")
+uploadFileInputs.set_AccessToken("phgmf2fgdv8rge8u")
+uploadFileInputs.set_AccessTokenSecret("q4qynhfgileze17")
+
 uploadFileInputs.set_FileName(str(sys.argv[1]))
-uploadFileInputs.set_AccessTokenSecret("yourTokenSecret")
-uploadFileInputs.set_AppKey("yourAppKey")
 uploadFileInputs.set_FileContents(encoded_string)
 uploadFileInputs.set_Root("sandbox")
 
